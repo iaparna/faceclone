2f4928694dc71570bbb38240c1a89046c9ddafd0
iaparna <aparnasahoo120@gmail.com>
2021-10-01 11:23:46 +0530

    fourth commit

--- first_project/settings.py
+++ first_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e4i8v8dj+6(=2*@k(mpcdq1%&ll8(pp8!c3uwi!4d)fxad6t75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
